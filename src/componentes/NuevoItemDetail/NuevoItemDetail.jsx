7fb005ec6ddc3bdfc5d9f2f7524c10e1767f6e6d
anais <anaislm1212@gmail.com>
2024-03-09 22:24:29 -0300

    2da_preE

--- src/componentes/NuevoItemDetail/NuevoItemDetail.jsx
+++ src/componentes/NuevoItemDetail/NuevoItemDetail.jsx
@@ -4,12 +4,12 @@ const NuevoItemDetail = ({id, name, price, category, img, description, stock}) =
 
    
     return (
-      <article>
+      <article style={{borderStyle:"inset", borderRadius:"10px", margin:"8px"}}>
           <h1>{name}</h1>
           <img src={img} style={{width:300}}/>
-          <p>Categoría: {category}</p>
-          <p>Descripción: {description}</p>
-          <h1>{price}</h1>
+          <h2 style={{fontWeight: "bold", marginTop:"15px"}}>Categoría: {category}</h2>
+          <p style={{textAlign: "justify", marginTop:"15px",  marginLeft:"15px", maxWidth:"600px"}}>Descripción: {description}</p>
+          <h3 style={{fontWeight: "bold", marginTop:"15px"}}>Precio: ${price} UYU</h3>
           <ItemContador stock={stock}/>
       </article>
     )
